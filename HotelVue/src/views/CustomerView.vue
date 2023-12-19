9b85856d0110a2a85dc770fa19c04e5a97ba0336
Lingwuxin <57278566+Lingwuxin@users.noreply.github.com>
2023-12-19 18:05:45 +0800

    完善登记功能

--- HotelVue/src/views/CustomerView.vue
+++ HotelVue/src/views/CustomerView.vue
@@ -3,22 +3,30 @@
       <table class="table">
         <thead>
           <tr>
+            <th>ID</th>
             <th>姓名</th>
             <th>邮箱</th>
             <th>电话</th>
             <th>卡号</th>
             <th>操作</th>
+            <th>修改</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, index) in data" :key="index">
-            <td>{{ item.customerID + item.firstName + item.lastName }}</td>
+            
+            <td>{{ item.customerID }}</td>
+            <td>{{ item.firstName + item.lastName }}</td>
             <td>{{ item.email }}</td>
             <td>{{ item.phone }}</td>
-            <td>{{ item.creditCardInfo }}</td>
+            <td>{{ item.creditCardInfo }}</td>  
             <td>
               <!-- 使用 v-if 控制按钮的显示 -->
-              <button v-if="item.status !== 'checkout'" @click="() => checkout(item.customerID, index)">退房{{ item.status }}</button>
+              <button v-if="item.status !== 'checkout'" @click="() => checkout(item.customerID, index)">退房</button>
+              <span v-if="item.status =='checkout'">已退房</span>
+            </td>
+            <td>
+              <button>修改</button>
             </td>
           </tr>
         </tbody>
@@ -41,7 +49,18 @@
   }
   
   const data = ref<Customer[]>([]);
-  
+  const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/customer');
+    data.value = response.data.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
   onMounted(async () => {
     try {
       const response = await axios.get('http://localhost:8080/customer');
@@ -55,8 +74,9 @@
     console.log('Checking out with customerId:', customerId);
     try {
       // 发送退房请求，将顾客的唯一标识符放在 URL 中
-      await axios.delete(`http://localhost:8080/checkout/${customerId}`);
+      await axios.post(`http://localhost:8080/checkout/${customerId}`);
       // 在页面上移除对应的顾客数据
+      await fetchData();
     } catch (error) {
       console.error('Error checking out:', error);
     }
